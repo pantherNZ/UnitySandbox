eea194cc20ffdee940d8a529f368908a23dcd2af
Alex Denford <pantherNZ@users.noreply.github.com>
2021-08-28 14:37:03 +1200

    Elevator system, fixing various bugs and implementing states

--- Project/Assets/Scripts/ElevatorHandler.cs
+++ Project/Assets/Scripts/ElevatorHandler.cs
@@ -8,7 +8,10 @@ public class ElevatorHandler : MonoBehaviour
 {
     public enum State
     {
-        Stationary,
+        StationaryDoorsClosed,
+        StationaryDoorsOpening,
+        StationaryDoorsClosing,
+        StationaryDoorsOpen,
         MovingUp,
         MovingDown,
     }
@@ -16,7 +19,6 @@ public class ElevatorHandler : MonoBehaviour
     [SerializeField] float elevatorSpeed = 10.0f;
     [SerializeField] float doorOpenWidth = 2.0f;
     [SerializeField] float doorOpenSpeed = 1.0f;
-    [SerializeField] bool allowChangingDirectionWhenOccupied = false;
 
     [Serializable]
     public class FloorData
@@ -31,7 +33,7 @@ public class ElevatorHandler : MonoBehaviour
     [SerializeField] List<FloorData> floors = new List<FloorData>();
 
     Int32 currentFloor = 0;
-    State state = State.Stationary;
+    State state = State.StationaryDoorsClosed;
     List<Int32> internalRequests = new List<Int32>();
     List<Pair<Int32, State>> externalRequests = new List<Pair<Int32, State>>();
 
@@ -52,16 +54,26 @@ public class ElevatorHandler : MonoBehaviour
     {
         if( floors.IsEmpty() )
             return;
+        if( currentFloor == floorIndex )
+        {
+            if( state == State.StationaryDoorsClosed )
+                StartCoroutine( MoveDoors( true ) );
+            return;
+        }
         if( internalRequests.Contains( floorIndex ) )
             return;
 
         internalRequests.Add( floorIndex );
-        ProcessNextMovement();
+
+        if( !IsMoving() )
+            ProcessNextMovement();
     }
 
     public void CallElevatorUpFromFloor( Int32 floorIndex )
     {
         if( floors.IsEmpty() )
+            return;
+        if( currentFloor == floorIndex && ( state == State.StationaryDoorsOpen || state == State.StationaryDoorsOpening ) )
             return;
 
         var request = new Pair<Int32, State>() { First = floorIndex, Second = State.MovingUp };
@@ -76,6 +88,8 @@ public class ElevatorHandler : MonoBehaviour
     {
         if( floors.IsEmpty() )
             return;
+        if( currentFloor == floorIndex && ( state == State.StationaryDoorsOpen || state == State.StationaryDoorsOpening ) )
+            return;
 
         var request = new Pair<Int32, State>() { First = floorIndex, Second = State.MovingDown };
         if( externalRequests.Contains( request ) )
@@ -87,7 +101,7 @@ public class ElevatorHandler : MonoBehaviour
 
     public bool IsMoving()
     {
-        return state != State.Stationary;
+        return state == State.MovingUp || state == State.MovingDown;
     }
 
     public State GetState()
@@ -105,9 +119,8 @@ public class ElevatorHandler : MonoBehaviour
         if( floors.IsEmpty() || currentFloor >= floors.Count - 1 )
             return;
 
-        ++currentFloor;
-        var distance = Mathf.Abs( floors[currentFloor].height - floors[currentFloor - 1].height );
-        StartCoroutine( MoveTo( true, transform.position.SetY( floors[currentFloor].height ), distance / elevatorSpeed ) );
+        var distance = Mathf.Abs( floors[currentFloor + 1].height - floors[currentFloor].height );
+        StartCoroutine( MoveTo( true, transform.position.SetY( floors[currentFloor + 1].height ), distance / elevatorSpeed ) );
     }
 
     private void MoveDown()
@@ -115,58 +128,63 @@ public class ElevatorHandler : MonoBehaviour
         if( floors.IsEmpty() || currentFloor <= 0 )
             return;
 
-        --currentFloor;
-        var distance = Mathf.Abs( floors[currentFloor].height - floors[currentFloor + 1].height );
-        StartCoroutine( MoveTo( false, transform.position.SetY( floors[currentFloor].height ), distance / elevatorSpeed ) );
+        var distance = Mathf.Abs( floors[currentFloor - 1].height - floors[currentFloor].height );
+        StartCoroutine( MoveTo( false, transform.position.SetY( floors[currentFloor - 1].height ), distance / elevatorSpeed ) );
     }
 
     private IEnumerator MoveTo( bool movingUp, Vector3 targetPos, float duration )
     {
+        if( state == State.StationaryDoorsOpen )
+            yield return MoveDoors( false );
+
         state = movingUp ? State.MovingUp : State.MovingDown;
         yield return Utility.InterpolatePosition( transform, targetPos, duration );
+        currentFloor += ( movingUp ? 1 : -1 );
+        state = State.StationaryDoorsClosed;
         ProcessNextMovement();
     }
 
     private void ProcessNextMovement()
     {
-        if( !IsMoving() )
+        if( IsMoving() )
+            return;
+
+        // Stop if we have any internal requests for this floor or any external ones at this floor in the same direction that we are moving
+        if( internalRequests.Any( x => x == currentFloor ) ||
+            externalRequests.Any( x => x.First == currentFloor && ( internalRequests.IsEmpty() || x.Second == state ) ) )
         {
-            // Serve next internal request
-            if( !internalRequests.IsEmpty() )
-            {
-                if( currentFloor > internalRequests.Front() )
-                    MoveDown();
-                else if( currentFloor < internalRequests.Front() )
-                    MoveUp();
-            }
-            // Serve next external request
-            else if( !externalRequests.IsEmpty() )
-            {
-                if( currentFloor > externalRequests.Front().First )
-                    MoveDown();
-                else if( currentFloor < externalRequests.Front().First )
-                    MoveUp();
-            }
-            else
-            {
-                // After 5 sec of no requests, rest back to ground floor
-                Utility.FunctionTimer.CreateTimer( 5.0f, () =>
-                {
-                    if( !IsMoving() && 
-                        internalRequests.IsEmpty() &&
-                        externalRequests.IsEmpty() )
-                    {
-                        CallElevatorFromInside( 0 );
-                    }
-                } );
-            }
+            StartCoroutine( StopAtFloor() );
+            return;
         }
-        else
+
+        // Serve next internal request
+        if( !internalRequests.IsEmpty() )
         {
-            // Stop if we have any internal requests for this floor or any external ones at this floor in the same direction that we are moving
-            if( internalRequests.Any( x => x == currentFloor ) ||
-                externalRequests.Any( x => x.First == currentFloor && ( internalRequests.IsEmpty() || x.Second == state ) ) )
-                StartCoroutine( StopAtFloor() );
+            if( currentFloor > internalRequests.Front() )
+                MoveDown();
+            else if( currentFloor < internalRequests.Front() )
+                MoveUp();
+        }
+        // Serve next external request
+        else if( !externalRequests.IsEmpty() )
+        {
+            if( currentFloor > externalRequests.Front().First )
+                MoveDown();
+            else if( currentFloor < externalRequests.Front().First )
+                MoveUp();
+        }
+        else if( currentFloor != 0 )
+        {
+            // After 5 sec of no requests, rest back to ground floor
+            Utility.FunctionTimer.CreateTimer( 5.0f, () =>
+            {
+                if( !IsMoving() && 
+                    internalRequests.IsEmpty() &&
+                    externalRequests.IsEmpty() )
+                {
+                    CallElevatorFromInside( 0 );
+                }
+            } );
         }
     }
 
@@ -178,15 +196,21 @@ public class ElevatorHandler : MonoBehaviour
         internalRequests.Remove( currentFloor );
         externalRequests.Remove( x => x.First == currentFloor && ( internalRequests.IsEmpty() || x.Second == state ) );
        
-        state = State.Stationary;
         yield return MoveDoors( false );
         ProcessNextMovement();
     }
 
     private IEnumerator MoveDoors( bool open )
     {
+        if( open && state != State.StationaryDoorsClosed )
+            yield return null;
+        if( !open && state != State.StationaryDoorsOpen )
+            yield return null;
+
+        state = open ? State.StationaryDoorsOpening : State.StationaryDoorsClosing;
         StartCoroutine( MoveDoors( leftDoor.transform, rightDoor.transform, open ) );
         yield return MoveDoors( floors[currentFloor].leftDoor.transform, floors[currentFloor].rightDoor.transform, open );
+        state = open ? State.StationaryDoorsOpen : State.StationaryDoorsClosed;
     }
 
     private IEnumerator MoveDoors( Transform left, Transform right, bool open )
